fbf4903dd4ac178db6aa1a159d1c63d91fd42dd9
kentguo_1997 <guoyunkai201709@gmail.com>
2022-02-22 14:14:48 +0800

    feat: add body-parser and set the function of determining if the input email & password is correct

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const accountModel = require('./models/account')
+const account = require('./models/account')
 
 const port = 3001
 
@@ -29,9 +33,39 @@ app.set( 'view engine', 'hbs' )
 
 
 
+// default use setting
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+
 // setting routes
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+
+app.post('/accounts', (req, res) => {
+  const inputEmail = req.body.inputEmail
+  const inputPassword = req.body.inputPassword
+  let matchedAccount = []
+  
+  accountModel.find()
+    .lean()
+    .then( accounts => {
+      // determine if the input email exists 
+      matchedAccount = accounts.filter( account => account.email === inputEmail )
+      if (matchedAccount.length === 0) {
+        return console.log('Wrong Email!')
+      } else {
+        // determine if the input password is correct
+        if (matchedAccount[0].password === inputPassword) {
+          res.render('success', {firstName: matchedAccount[0].firstName})
+        } else {
+          return console.log('Wrong Password!')
+        }
+      } 
+    })
 })
 
 
